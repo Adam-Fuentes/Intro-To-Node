4a017e69332037b29721f22741dc94c3fd578da9
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-10-29 09:24:36 +0100

    add npm supervillains

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const superheroes = require('superheroes');
 var mySuperHeroName = superheroes.random();
 
 console.log("Mi nombre de super hero es: "+ mySuperHeroName);
+
+const supervillains = require('supervillains');
+
+var myVillainName = supervillains.random();
+
+console.log("Mi nombre de villano es: "+ myVillainName);
