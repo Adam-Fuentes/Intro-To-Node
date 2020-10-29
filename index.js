920b1f2f4a0400e38953f3eda5e90ca9e5a9670b
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-10-29 09:17:02 +0100

    add npm superhroes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const fs = require("fs");
 
-fs.copyFileSync("file1.txt", "file2.txt");
+const superheroes = require('superheroes');
+
+var mySuperHeroName = superheroes.random();
+
+console.log("Mi nombre de super hero es: "+ mySuperHeroName);
